0c79f156502caa13d0068849e788b747e87edd8c
damiansucre <damian_escalante_83@hotmail.com>
2023-06-16 01:23:53 -0300

    second-update-week2

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,27 @@ document.getElementById('current-year').textContent = new Date().toLocaleDateStr
 document.getElementById('current-date').textContent = new Date().toLocaleDateString('en-US',date); */
 
 document.getElementById('current-date').textContent=(document.lastModified);
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("header");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#153243";
+		main.style.color = "#B4B8AB";
+		modeButton.textContent = "☑️";
+	}
+});
