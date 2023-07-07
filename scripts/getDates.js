f8e1b0eff3b293e577698f51284b40d6c35cbc02
damiansucre <damian_escalante_83@hotmail.com>
2023-07-07 01:43:44 -0300

    week-5-update-3

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -51,11 +51,11 @@ localStorage.setItem("numVisits-ls", numVisits); */
 
 /*Week 04 Team Activity*/
 
-const kp1 = document.querySelector("#password");
+/* const kp1 = document.querySelector("#password");
 const kp2 = document.querySelector("#password2");
 const message = document.querySelector("#formmessage");
 
-kp2.addEventListener("focusout", checkSame);
+kp2.addEventListener("focusout", checkSame); */
 
 // This should be refactored.
 function checkSame() {
@@ -78,9 +78,45 @@ const range = document.getElementById("r");
 
 console.log(rangevalue)
 // RANGE event listener
-range.addEventListener('change', displayRatingValue);
-range.addEventListener('input', displayRatingValue);
+//range.addEventListener('change', displayRatingValue);
+//range.addEventListener('input', displayRatingValue);
 
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
+
+//weather
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url ='https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=4473a14fe77c4a9fba6d398101963181&units=imperial';
+
+
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }else{
+            throw error(await response.text());
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+function displayResults(data){
+    currentTemp.innerHTML = `${data.main.temp} &deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    console.log(iconsrc);
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('src', iconsrc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
