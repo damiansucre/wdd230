202478fbe67e1333e04bfb5bb40cee8fb6ec473d
damiansucre <damian_escalante_83@hotmail.com>
2023-07-10 17:04:53 -0300

    week-5-update-8

--- chamber/scripts/get-information.js
+++ chamber/scripts/get-information.js
@@ -73,3 +73,81 @@ let findSelected = () =>{
 radioBtns.forEach(radioBtns =>{
 	radioBtns.addEventListener("change", findSelected);
 });
+
+/*Script for JSON Directory Page*/
+
+const cards = document.querySelector('.grid');
+const baseURL = "https://damiansucre.github.io/wdd230/chamber/";
+const membersURL = "https://damiansucre.github.io/wdd230/chamber/data/members.json";
+
+async function getMembers(){
+    try{
+        const response = await fetch(membersURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayMembers(data.companies);
+        }else{
+            throw error(await response.text());
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+const displayMembers = (companies)=>{
+    companies.forEach((companie) => {
+        let card= document.createElement('section');
+        let name = document.createElement('h2');
+		let image = document.createElement('img');
+        let address = document.createElement('h3');
+        let phone = document.createElement('h3');
+		let membership = document.createElement('h3');
+		let url = document.createElement('a');
+       
+
+        name.textContent = `${companie.name}`;
+        address.textContent = `Address: ${companie.address}`;
+        phone.textContent = `Phone: ${companie.phone}`;
+		url.textContent = `${companie.url}`;
+		url.setAttribute('href', companie.url);
+		membership.textContent = `Membership: ${companie.membership}`;
+        image.setAttribute('src', companie.image);
+        image.setAttribute('loading', 'lazy');
+        //image.setAttribute('width', '340');
+        //image.setAttribute('height', '440');
+
+        card.appendChild(name);
+		card.appendChild(image);
+        card.appendChild(address);
+        card.appendChild(phone);
+		card.appendChild(url);
+		card.appendChild(membership);
+       
+        cards.appendChild(card);
+    });
+}
+
+getMembers();
+
+//Let the user toggle between a "grid" type view of member cards or a simple, one-column list of members
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
